cbda88ed7b1f5efb59694b12054ebb4252b00597
oncefirefly <oncefirefly@gmail.com>
2023-06-12 20:52:05 +0300

    feat: BPMap component (maplibre-gl) added

---
+++ src/components/BPMap.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { type Raw, markRaw, onMounted, shallowRef } from "vue";
+
+import maplibregl from "maplibre-gl";
+
+const map = shallowRef<Raw<unknown>>();
+const mapContainer = shallowRef();
+
+onMounted(() => {
+  map.value = markRaw(
+    new maplibregl.Map({
+      container: mapContainer.value,
+      style: "https://demotiles.maplibre.org/style.json",
+      center: [-74.5, 40],
+      zoom: 9,
+    })
+  );
+});
+</script>
+
+<template>
+  <div class="map" ref="mapContainer"></div>
+</template>
+
+<style>
+.map {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/MapView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import BPMap from "@/components/BPMap.vue";
+</script>
+
+<template>
+  <BPMap />
+</template>
+
+<style></style>
