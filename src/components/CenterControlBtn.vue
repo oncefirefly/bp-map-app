1124067ab8d2360295e8067a83637cf5064bcba2
oncefirefly <oncefirefly@gmail.com>
2023-06-15 04:02:59 +0300

    feat: Center Control button component added

---
+++ src/components/CenterControlBtn.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { Coordinates } from "@/types";
+
+const props = defineProps<{
+  text: string;
+  coordinates: Coordinates;
+}>();
+
+const emits = defineEmits<{
+  click: [coordinates: Coordinates];
+}>();
+
+const handleBtnClick = () => {
+  emits("click", props.coordinates);
+};
+</script>
+
+<template>
+  <button class="center-btn" type="button" @click="handleBtnClick">
+    {{ props.text }}
+  </button>
+</template>
+
+<style scoped>
+.center-btn {
+  padding: 10px;
+
+  text-transform: capitalize;
+  font-weight: 600;
+
+  border: none;
+  border-radius: 5px;
+
+  color: #fff;
+  background: #0096ff;
+}
+</style>
