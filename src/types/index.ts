5281386e395df81481c7d4cebecc768d9a0430e3
oncefirefly <oncefirefly@gmail.com>
2023-06-16 10:17:10 +0300

    feat: GeoJSON and PinballLocations types added

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,33 @@ export interface LayerControls {
 }
 
 export type Coordinates = [number, number];
+
+export interface GeoJSON {
+  type: string;
+  geometry: {
+    type: string;
+    coordinates: Coordinates;
+  };
+  properties: {
+    name: string;
+  };
+}
+
+export interface PinballLocations {
+  city: string;
+  country: string;
+  ic_active: unknown;
+  id: number;
+  is_stern_army: unknown;
+  last_updated_by_username: string;
+  lat: string;
+  location_type_id: number;
+  lon: string;
+  name: string;
+  num_machines: number;
+  operator_id: unknown;
+  state: string;
+  street: string;
+  zip: string;
+  zone_id: number;
+}
